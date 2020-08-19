fcf883cad3c15967a5a68d90c73db4161694339d
JuanHeis <juaany98@gmail.com>
2020-08-18 21:42:54 -0300

    Router!

---
+++ js/routes.js
@@ -0,0 +1,15 @@
+const PATHS = {
+    home:{
+        path:"/",
+        template: '<h1> Home </h1>'
+    },
+    
+    about:{
+        path:"/about",
+        template: '<h1> Sobre mi </h1>'
+    },
+    contact:{
+        path:"/contact",
+        template: '<h1> Contacto </h1>'
+    }
+}
